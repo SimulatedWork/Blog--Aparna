26647e52a076c2c8f2ca10f3f5b517aa32c9fbc5
Aparnaa-k <aparnaa.kumar@kalvium.community>
2023-10-25 10:22:40 +0530

    initialized the react app and done with navbar and mainpage components

---
+++ src/components/blogs/blogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./blogs.css";
+
+function blogs() {
+  return (
+    <div>
+      <h1>Blogs</h1>
+    </div>
+  )
+}
+
+export default blogs
---
+++ src/components/register/register.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar2 from '../navbar2/navbar2'
+
+function register() {
+  return (
+    <>
+      <Navbar2 />
+      <div>
+        <h1>Register</h1>
+      </div>
+    </>
+  );
+}
+
+export default register
