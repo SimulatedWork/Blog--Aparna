99a559eecc3d92ad1636eb16441d90016896899c
Aparnaa-k <aparnaa.kumar@kalvium.community>
2023-11-28 18:35:13 +0530

    minor changes in backend

--- backend/models/createUserTable.js
+++ backend/models/createUserTable.js
@@ -9,8 +9,7 @@ const createUserTable = async () => {
         id SERIAL PRIMARY KEY,
         username VARCHAR(50) UNIQUE NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        
+        password VARCHAR(255) NOT NULL        
       );
     `);
 
